8cd45fc2175341d8b36ee1313965c7efbfd63473
michaelvenegas <50221114+michaelvenegas@users.noreply.github.com>
2021-09-08 01:00:53 -0300

    App version 1.0

---
+++ app/src/main/java/MichaelVenegas/miprimeraapp/MainActivity.java
@@ -0,0 +1,14 @@
+package MichaelVenegas.miprimeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
